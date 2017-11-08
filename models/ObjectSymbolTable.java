364d6eaf9787e993c952298c3d4abdee1e154a64
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2017-11-08 19:08:56 -0200

    Modificação da classe ObjectSymbolTable.java, ou seja, foi adicionado outros atributos.

--- models/ObjectSymbolTable.java
+++ models/ObjectSymbolTable.java
@@ -5,13 +5,16 @@ package models;
  */
 public class ObjectSymbolTable {
 	// DECLARAÇÃO DE CONSTANTES A SEREM USADAS PARA INDENTIFICAÇÃO DO OBJETO A SER SALVO NA TABELA DE SIMBOLOS
-	static final int VARIABLE = 1, CONSTANT = 2, FUNCTION = 3, PARAMETER = 4;
-	static final int VOID = 0, INT = 1, FLOAT = 2, STRING = 3, BOOL = 4;
+	public static final int GLOBAL = 1, LOCAL = 2;
+	public static final int VARIABLE = 1, CONSTANT = 2, FUNCTION = 3, PARAMETER = 4;
+	public static final int VOID = 0, INT = 1, FLOAT = 2, STRING = 3, BOOL = 4;
 	
 	private int type;
+	private int scope;
 	private int memoryAddress;
-	private int typeObjectSimbolTable;
-	private Object valueObject;
+	private int typeObjectSimbolTable; 
+	private Object valueObject; // valor atribuido ao objeto (varivel ou constante ou parametro)
+	private String visibleScopeFunctionName; //nome função onde o objeto(que não é uma função) é visivel caso ela seja local
 	
 	public int getType() {
 		return type;
@@ -45,9 +48,27 @@ public class ObjectSymbolTable {
 		this.valueObject = valueObject;
 	}
 	
-	public ObjectSymbolTable(int memoryAddress, int type, int typeObjectSimbolTable, Object valueObject) {
+	public int getScope() {
+		return scope;
+	}
+
+	public void setScope(int scope) {
+		this.scope = scope;
+	}
+
+	public String getVisibleScopeFunctionName() {
+		return visibleScopeFunctionName;
+	}
+
+	public void setVisibleScopeFunctionName(String visibleScopeFunctionName) {
+		this.visibleScopeFunctionName = visibleScopeFunctionName;
+	}
+	
+	public ObjectSymbolTable(int typeObjectSimbolTable, int type, int scope, String visibleScopeFunctionName, int memoryAddress, Object value) {
 		this.typeObjectSimbolTable = typeObjectSimbolTable;
 		this.type = type;
+		this.scope = scope;
+		this.visibleScopeFunctionName = visibleScopeFunctionName;
 		this.memoryAddress = memoryAddress;
 		this.valueObject = valueObject;
 	}
