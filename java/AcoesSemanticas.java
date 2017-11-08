8e11d72d663cfe60a55e57b0549ec09d524dc333
penafortdias <marcopenaforttj@hotmail.com>
2017-11-08 11:34:44 -0300

    Pacote JMRCompiler.java criado

---
+++ java/AcoesSemanticas.java
@@ -0,0 +1,34 @@
+package JMRCompiler.java;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import JMRCompiler.grammar.JMRCompilerBaseListener;
+import JMRCompiler.grammar.JMRCompilerBaseListener;
+import JMRCompiler.grammar.JMRCompilerParser;
+
+public class AcoesSemanticas extends JMRCompilerBaseListener {
+    
+    private Map<String, Integer> tabSimb = new HashMap<String, Integer>();    
+
+    public Map<String, Integer> getTabSimb() {
+        return tabSimb;
+    }
+    public void setTabSimb(Map<String, Integer> tabSimb) {
+        this.tabSimb = tabSimb;
+    }
+
+//	public void exitPrograma(AnaSintParser.ProgramaContext ctx) { 
+//            System.out.println("Tabela de ");
+//        }
+    
+	public void exitDecVars(JMRCompilerParser.DecVarsContext ctx) {
+            for (TerminalNode id: ctx.listaIDs().ID()) {
+                if (!tabSimb.containsKey(id.getText())) 
+                    tabSimb.put(id.getText(),ctx.tipo.t);
+                else System.out.println("ID duplicado");
+            } 
+        }
+    
+}
