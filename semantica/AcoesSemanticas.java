7b2dce5dadde78f75d82788e8db1e68cb9e948eb
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2017-11-13 15:42:52 -0200

    Modificação e implementação de metodo para a verificação da declaração de funções.

--- semantica/AcoesSemanticas.java
+++ semantica/AcoesSemanticas.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import grammar.*;
+import models.Constant;
 import models.ObjectSymbolTable;
+import models.Variable;
 
 public class AcoesSemanticas extends JMRCompilerBaseListener {
 	
-	private int addressMemoryFree = 0; // possivel erro ao armazenar o endereço de uma variavel
+	private int addressMemoryFree = 0; // VERIFICAR SE A ERRO NA GERAÇÃO DO ENDENREÇO DE MEMORIA SEQUENCIAL (POSSIVEL ERRO AO ARMAZENAR)
     private Map<String, ObjectSymbolTable> symbolTable = new HashMap<String, ObjectSymbolTable>();    
 
     public Map<String, ObjectSymbolTable> getSymbolTable() {
@@ -29,7 +31,12 @@ public class AcoesSemanticas extends JMRCompilerBaseListener {
 	public void exitDecVars(JMRCompilerParser.DecVarsContext ctx) {
 		for(TerminalNode id: ctx.listaIDs().ID()) {
 			if(!symbolTable.containsKey(id.getText())) {
-				ObjectSymbolTable variable = new ObjectSymbolTable(ObjectSymbolTable.VARIABLE, ctx.tipo.type, ObjectSymbolTable.GLOBAL, null, addressMemoryFree++, null);
+				ObjectSymbolTable variable = new Variable(Variable.VARIABLE, addressMemoryFree++);
+				
+				((Variable)variable).setType(ctx.tipo.type);
+				((Variable)variable).setScope(Variable.GLOBAL);
+				((Variable)variable).setValueObject(null);
+				
 				
 				symbolTable.put(id.getText(), variable);
 			} else {
@@ -39,15 +46,24 @@ public class AcoesSemanticas extends JMRCompilerBaseListener {
 		}
 	}
 	
+	/**
+	 * @description: SALVA AS CONSTANTES DECLARADAS NA TABELA DE SIMBOLOS
+	 */
 	public void exitDecConsts(JMRCompilerParser.DecConstsContext ctx) {
 		
 		if(!symbolTable.containsKey(ctx.ID().getText())) {
 			if((ctx.tipo().type == 1)&&(ctx.valor().value instanceof Integer) || (ctx.tipo().type == 2)&&(ctx.valor().value instanceof Float) || (ctx.tipo().type == 3)&&(ctx.valor().value instanceof String) || (ctx.tipo().type == 4)&&(ctx.valor().value instanceof Boolean)) {
-				ObjectSymbolTable constant = new ObjectSymbolTable(ObjectSymbolTable.CONSTANT, ctx.tipo().type, ObjectSymbolTable.GLOBAL, null, addressMemoryFree++, ctx.valor().value);
+				ObjectSymbolTable constant = new Constant(ObjectSymbolTable.CONSTANT, addressMemoryFree++);
+				
+				((Constant)constant).setType(ctx.tipo().type);
+				((Constant)constant).setValueObject(ctx.valor().value);
 				
 				symbolTable.put(ctx.ID().getText(), constant);
 			} else if((ctx.tipo().type == 2)&&(ctx.valor().value instanceof Integer)){
-				ObjectSymbolTable constant = new ObjectSymbolTable(ObjectSymbolTable.CONSTANT, ctx.tipo().type, ObjectSymbolTable.GLOBAL, null, addressMemoryFree++, (float)((int)ctx.valor().value));
+				ObjectSymbolTable constant = new Constant(ObjectSymbolTable.CONSTANT, addressMemoryFree++);
+				
+				((Constant)constant).setType(ctx.tipo().type);
+				((Constant)constant).setValueObject((float)((int)ctx.valor().value));
 				
 				symbolTable.put(ctx.ID().getText(), constant);
 			}else {
@@ -59,6 +75,17 @@ public class AcoesSemanticas extends JMRCompilerBaseListener {
 			System.out.println("ERRO: o nome especificado para a constante ja foi declarado. Linha " + ctx.ID().getSymbol().getLine());
 		}
 		
+	}
+	
+	/**
+	 * @description: SALVA E ARMAZENA NA TABELA DE SIMBOLOS UMA FUNÇÃO DECLARADA
+	 */
+	public void exitDecFuncs(JMRCompilerParser.DecFuncsContext ctx) {
+		if(!symbolTable.containsKey(ctx.ID().getText())) {
+			// VERIFICAR SE TIPO DE RETORNO CORRESPONDE COM O TIPO SENDO RETORNADO PARA FUNÇÕES QUE NÃO SÃO VOIDS
+		} else {
+			
+		}
 	}
     
 }
