4664ea1e32c93d7716c82c6a3b3f3233d859708f
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2017-11-20 15:46:40 -0200

    Ateração do arquivo principal, usado para teste de execução. E modificação no arquivo de entrada.

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import controller.SemanticActions;
 import grammar.*;
 
 import grammar.JMRCompilerLexer;
-import semantica.AcoesSemanticas;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -24,7 +25,7 @@ public class Main {
 		JMRCompilerParser parser = new JMRCompilerParser(tokens);
 		ParseTree tree = parser.programa();
       
-		AcoesSemanticas listener = new AcoesSemanticas();
+		SemanticActions listener = new SemanticActions(file);
 		ParseTreeWalker walker = new ParseTreeWalker();
 		walker.walk(listener, tree);
       
